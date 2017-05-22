d4d8254c0845265cd0137a38ca6b00ba75b9141e
Brad Rydzewski <brad.rydzewski@gmail.com>
2017-05-22 16:02:41 +0200

    add bottle:unneeded
    as described in http://octavore.com/posts/2016/02/15/distributing-go-apps-os-x

--- drone.rb
+++ drone.rb
@@ -7,6 +7,8 @@ class Drone < Formula
   url "https://github.com/drone/drone-cli/releases/download/v0.6.0/drone_darwin_amd64.tar.gz"
   sha256 "c97670fae9202694efc3f72b409ce818976e9b4f7946d2826faabc6133ba969e"
 
+  bottle :unneeded
+  
   def install
       bin.install "#{buildpath}/drone" => "drone"
     end
