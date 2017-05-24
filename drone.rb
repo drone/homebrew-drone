b5e59e94457471dbaf6055c87b9522cadc98fbcd
Brad Rydzewski <brad.rydzewski@gmail.com>
2017-05-24 20:56:48 +0200

    Update drone.rb

--- drone.rb
+++ drone.rb
@@ -4,8 +4,8 @@ require "securerandom"
 class Drone < Formula
   homepage "https://github.com/drone/drone"
   head "https://github.com/drone/drone.git"
-  url "https://github.com/drone/drone-cli/releases/download/v0.6.0/drone_darwin_amd64.tar.gz"
-  sha256 "c97670fae9202694efc3f72b409ce818976e9b4f7946d2826faabc6133ba969e"
+  url "https://github.com/drone/drone-cli/releases/download/v0.7.0/drone_darwin_amd64.tar.gz"
+  sha256 "253f415a55dbc4f01cb8736ab7ce414997f3101cddcb5bcc82b0b050b8a7a69b"
 
   bottle :unneeded
   
