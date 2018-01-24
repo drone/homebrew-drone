8441a7a4d4f0fa57aff8a9c4f6a6ba019edb9f9b
Jacob McCann <jacob.mccann2@target.com>
2018-01-24 09:27:37 -0600

    Update version 0.8 to 0.8.1 and add versions 0.8.0 and 0.8.1

--- Formula/drone@0.8.1.rb
+++ Formula/drone@0.8.1.rb
@@ -1,8 +1,8 @@
 require File.expand_path("../../Abstract/abstract-drone", __FILE__)
 
-class DroneAT08 < AbstractDrone
+class DroneAT081 < AbstractDrone
   init
   url "https://github.com/drone/drone-cli/releases/download/v0.8.1/drone_darwin_amd64.tar.gz"
   sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v0.8.1/drone_checksums.txt`.split(' ').first
-  version "0.8"
+  version "0.8.1"
 end
