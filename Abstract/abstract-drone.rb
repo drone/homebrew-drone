16144511603d56f2baf577b085a5c3c92a5c28ca
James Gee <James.gee@digital.homeoffice.gov.uk>
2018-02-21 11:15:32 +0000

    Add support for linux brew

--- Abstract/abstract-drone.rb
+++ Abstract/abstract-drone.rb
@@ -4,12 +4,20 @@ class AbstractDrone < Formula
   def self.init
     homepage "https://github.com/drone/drone-cli"
     head "https://github.com/drone/drone-cli.git"
-    url assets.select { |v| File.basename(v['browser_download_url']) == 'drone_darwin_amd64.tar.gz' }.first['browser_download_url']
+    url assets.select { |v| File.basename(v['browser_download_url']) == "#{platform_url}" }.first['browser_download_url']
     sha256 sha256sum
 
     test do
       system "#{bin}/drone", "--version"
     end
+  end
+
+  def self.platform_url
+    p = 'drone_darwin_amd64.tar.gz'
+    if !RUBY_PLATFORM.downcase.include?('darwin')
+      p = 'drone_linux_amd64.tar.gz'
+    end
+    p
   end
 
   def self.curl_cmd
@@ -40,7 +48,7 @@ class AbstractDrone < Formula
       raise "Could not find checksum"
     end
     url = checksum_assest.first['browser_download_url']
-    `curl -L -s #{url}`.lines.grep(/drone_darwin_amd64.tar.gz/).first.split(' ').first
+    `curl -L -s #{url}`.lines.grep(/#{platform_url}/).first.split(' ').first
   end
 
   def install
