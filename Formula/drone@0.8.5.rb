b0de879f9ba76186afaacb400491cd3316740d0c
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-03-09 16:07:44 -0800

    bump version to 0.8.5

---
+++ Formula/drone@0.8.5.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../../Abstract/abstract-drone", __FILE__)
+
+class DroneAT085 < AbstractDrone
+  version "0.8.5"
+  init
+end
--- Formula/drone@0.8.rb
+++ Formula/drone@0.8.rb
@@ -3,6 +3,6 @@ require File.expand_path("../../Abstract/abstract-drone", __FILE__)
 class DroneAT08 < AbstractDrone
   version "0.8"
   init
-  url "https://github.com/drone/drone-cli/releases/download/v0.8.4/drone_darwin_amd64.tar.gz"
-  sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v0.8.4/drone_checksums.txt`.split(' ').first
+  url "https://github.com/drone/drone-cli/releases/download/v0.8.5/drone_darwin_amd64.tar.gz"
+  sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v0.8.5/drone_checksums.txt`.split(' ').first
 end
