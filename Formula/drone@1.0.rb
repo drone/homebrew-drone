6b22a3089d0a0815b8b3c915ff48b34f15dbddec
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-04-22 17:09:14 -0700

    Update drone@1.0.rb

--- Formula/drone@1.0.rb
+++ Formula/drone@1.0.rb
@@ -3,6 +3,6 @@ require File.expand_path("../../Abstract/abstract-drone", __FILE__)
 class DroneAT10 < AbstractDrone
   version "1.0"
   init
-  url "https://github.com/drone/drone-cli/releases/download/v1.0.7/drone_darwin_amd64.tar.gz"
-  sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v1.0.7/drone_checksums.txt`.split(' ').first
+  url "https://github.com/drone/drone-cli/releases/download/v1.0.8/drone_darwin_amd64.tar.gz"
+  sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v1.0.8/drone_checksums.txt`.split(' ').first
 end
