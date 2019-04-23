6b634f8745a8fbe2b82885169c2e52a28720c925
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-04-22 17:10:27 -0700

    Create drone@1.1.rb

---
+++ Formula/drone@1.1.rb
@@ -0,0 +1,8 @@
+require File.expand_path("../../Abstract/abstract-drone", __FILE__)
+
+class DroneAT11 < AbstractDrone
+  version "1.1"
+  init
+  url "https://github.com/drone/drone-cli/releases/download/v1.1.0/drone_darwin_amd64.tar.gz"
+  sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v1.1.0/drone_checksums.txt`.split(' ').first
+end
