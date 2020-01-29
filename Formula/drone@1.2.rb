2a648c56f462a70b9917ce7a8faded417b3f0956
Brian Burnett <mail@hikerspath.com>
2020-01-29 15:26:21 -0500

    Update for v1.2.1

--- Formula/drone@1.2.rb
+++ Formula/drone@1.2.rb
@@ -3,6 +3,6 @@ require File.expand_path("../../Abstract/abstract-drone", __FILE__)
 class DroneAT12 < AbstractDrone
   version "1.2"
   init
-  url "https://github.com/drone/drone-cli/releases/download/v1.2.0/drone_darwin_amd64.tar.gz"
-  sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v1.2.0/drone_checksums.txt`.split(' ').first
+  url "https://github.com/drone/drone-cli/releases/download/v1.2.1/drone_darwin_amd64.tar.gz"
+  sha256 `curl -L -s https://github.com/drone/drone-cli/releases/download/v1.2.1/drone_checksums.txt`.split(' ').first
 end
