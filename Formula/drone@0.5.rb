bc78e9ab7fc66220bf58429bf30ac2bc2fc233c4
Diogo Campos <d.diogocampos@gmail.com>
2021-01-20 10:49:30 +0100

    Remove drone@0.5 formula

--- Formula/drone@0.5.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path("../../Abstract/abstract-drone", __FILE__)
-
-class DroneAT05 < AbstractDrone
-  version "0.5"
-  init
-  url "http://downloads.drone.io/0.5.0/release/darwin/amd64/drone.tar.gz"
-  sha256 `curl -s http://downloads.drone.io/0.5.0/release/darwin/amd64/drone.sha256`.split(' ').first
-end
