0e9afaf8cc91fdbb56b75243e154a11533e3091a
Brian Burnett <mail@hikerspath.com>
2021-09-08 12:26:35 -0400

    Add Missing Versions to v1.4.0

---
+++ Formula/drone@1.3.0.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../../Abstract/abstract-drone", __FILE__)
+
+class DroneAT130 < AbstractDrone
+  version "1.3.0"
+  init
+end
---
+++ Formula/drone@1.3.2.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../../Abstract/abstract-drone", __FILE__)
+
+class DroneAT132 < AbstractDrone
+  version "1.3.2"
+  init
+end
